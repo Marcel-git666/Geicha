c1a8b7577bc2d5b391fc1da9b4138b0f6b95f951
Marcel-git666 <93435045+Marcel-git666@users.noreply.github.com>
2022-03-14 16:29:14 +0100

    Initial Commit

---
+++ Geicha/GeichaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeichaApp.swift
+//  Geicha
+//
+//  Created by Marcel Mravec on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct GeichaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
